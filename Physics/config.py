511bf606f17d06f5621ea523161c5ff280152711
Ashutosh Tripathi <ashutoshtripathi6963@gmail.com>
2026-07-15 23:13:44 +0530

    Add simulation configuration settings

--- Physics/config.py
+++ Physics/config.py
@@ -1,1 +1,19 @@
-print("This code is Working !!!!")
+# config.py
+
+from constants import (
+    MAX_PRESSURE,
+    MIN_PRESSURE,
+    MAX_FLOW_RATE,
+    PIPE_DIAMETER,
+    WATER_DENSITY,
+    GRAVITY,
+)
+
+SIMULATION_CONFIG = {
+    "max_pressure": MAX_PRESSURE,
+    "min_pressure": MIN_PRESSURE,
+    "max_flow_rate": MAX_FLOW_RATE,
+    "pipe_diameter": PIPE_DIAMETER,
+    "water_density": WATER_DENSITY,
+    "gravity": GRAVITY,
+}
